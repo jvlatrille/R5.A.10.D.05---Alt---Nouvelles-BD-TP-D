3be9bb39758c41bd182d09b270f3b647193d65f7
Jules VINET LATRILLE <jvlatrille@iutbayonne.univ-pau.fr>
2025-11-28 14:22:16 +0100

    Question 8 corrigée + question 9

--- scripts/TD2/8.mongodb.js
+++ scripts/TD2/8.mongodb.js
@@ -1,5 +1,5 @@
 use("nodenot_bd1") ;
 
 db.Commandes.find({ 
-    "LignesDeCommande.3": { $exists: true } 
+    "LignesDeCommande.3": { $exists: true }  // 3 parce qu'on commence a 0
 });
--- scripts/TD2/9.mongodb.js
+++ scripts/TD2/9.mongodb.js
@@ -1,1 +1,15 @@
 use("nodenot_bd1") ;
+// Q9
+db.Articles.aggregate([
+    {
+        $project: {
+            Descriptif: 1,
+            NbCaracteres: { $strLenCP: "$Descriptif" } // Récupére la longueur
+        }
+    },
+    {
+        $match: {
+            NbCaracteres: { $gt: 40 }
+        }
+    }
+]);
